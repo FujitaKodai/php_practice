761f6a27c7adb416b80568951e822563f5d87028
Fujita Kodai <koudai.fu0818@gmail.com>
2022-12-10 00:03:46 +0000

    homework3 を追加

--- homework2.php
+++ homework2.php
@@ -2,41 +2,42 @@
 
 echo '課題１：';
 
-$name="FUJITA KOUDAI";
+$name = "FUJITA KOUDAI";
 
 if ($name == "FUJITA KOUDAI"){
     echo '私は',$name,'です';
-    echo"\n";
-} else{
-     echo "あなたの名前ではありません";
+    echo "\n";
+} else {
+    echo "あなたの名前ではありません";
   }
   
  echo '課題2：';
  $total = 0;
- for( $i=0; $i<=10000; $i++ ){
+ for( $i = 0 ; $i <= 10000 ; $i++ ){
      $total += $i; 
  }
  echo $total;
- echo"\n";
+ echo "\n";
  
   echo '課題3：';
-  echo"\n";
+  echo "\n";
  $fruits = array("apple" ,"orange", "grape", "banana", "peach");
  
- foreach($fruits as $fruits){
-     echo $fruits;
+ foreach( $fruits as $fruit) {
+     echo $fruit;
      echo "\n";
  }
  
    echo '課題4：';
-   echo"\n";
+   echo "\n";
   
-  $start=1;
-  $end =100;
+  $start = 1 ;
+  $end = 100 ;
   
-  for($i = $start; $i <= $end ;$i++){
+  for($i = $start ; $i <= $end ; $i++ ){
       if($i % 5 == 0){
           echo $i;
           echo "\n";
       }
   }
+  //１２月１０日メンタリングより再提出！！
---
+++ homework3.php
@@ -0,0 +1,78 @@
+<?php
+
+echo '課題１：';
+
+function double($x){
+    $result = 2 * $x;
+    return $result;
+}
+
+echo double(9);
+echo "\n";
+
+echo '課題2：';
+
+function add($a,$b){
+    $total = $a + $b ;
+    return $total ;
+}
+echo add(18,19);
+echo "\n";
+
+echo '課題3：';
+
+function maltiplication($arr){
+    $total= 1;
+    for( $i = 0; $i < count($arr) ; $i++){
+        $total *= $arr[$i];
+    }
+    return $total;
+}
+echo maltiplication(array(1,3,5,7,9));
+echo "\n";
+
+echo '課題4：';
+
+function max_array($arr){
+    $max_number = $arr[0];    // 初期値設定
+    foreach($arr as $a){
+        if ($a > $max_number){
+            $max_number = $a ;
+        }
+    }
+    return $max_number;
+}
+echo max_array(array(1,2,4,5,7,8,9,11,13,3,19));
+echo "\n";
+
+echo '課題5：';
+echo "\n";
+
+// strip_tagsを使ってみる・・・HTMLタグの除去
+$str="<h1>--strip_tagsを使ってみる--<h1>"."\n"
+."<p>HTMLタグの取り外し機能<p>"."\n"
+."<a href=>--strip_tags 終了--</a>"."\n";
+echo strip_tags($str) ;
+
+// array_pushを使ってみる・・・配列に複数の要素を追加したい場合に使う
+$array =array("~array_push開始","A","B","C");
+array_push($array,"D","E","array_push終了~"); 
+var_dump($array);
+
+// array_mergeを使ってみる・・・複数の配列を結合
+
+$array1 = [1,2,3];
+$array2 = [4,5,6];
+$array3 = [7,8,9];
+$array4 = array_merge($array1,$array2,$array3);
+print_r($array4);
+
+/*   time・・・現在時刻のUNIXタイムスタンプを取得
+　 mktime・・・指定した時刻のUNIXタイムスランプを取得*/
+　 
+　 $time1 = time();
+　 $time2 = time() + (3*24*60*60);
+　 echo "現在のUNIXタイムスランプ:".$time1;
+　 echo "３日後のUNIXタイムスランプ；".$time2;
+　 
+// date を使ってみる
